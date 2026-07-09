058c7e77829ad817b4a28a95d6bbdc034eda8664
jackyandrazat <jackyandrazat@gmail.com>
2026-07-09 15:27:54 +0700

    feat: integrate spatie/laravel-backup and add corresponding configuration, localization, and feature tests

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+use Illuminate\Support\Facades\Schedule;
+
+Schedule::command('backup:clean')->daily()->at('01:30');
+Schedule::command('backup:run --only-db')->daily()->at('01:00');
